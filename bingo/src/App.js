aef947a530533764085131d21082f12d6fe1d381
Aydn <aydnhooper1997@gmail.com>
2021-02-19 17:06:22 +0100

    complete
    finished bingo

---
+++ bingo/src/App.js
@@ -0,0 +1,71 @@
+import './App.css';
+import React, {useState, useEffect } from "react";
+import arrayShuffle from "shuffle-array"; //easy way to shuffle an array
+import Fireworks from './Fireworks';
+import { Data } from './Array';
+
+//use of arrayshuffle to randomise array 
+const shuffled = arrayShuffle(Data);
+
+//tile format
+function Tile({ id, children, onToggle, isSet }) {
+  return (
+    <div onClick={onToggle} className={`tile ${isSet ? "tile-set" : ""}`}>
+      {children}
+    </div>
+  );
+}
+
+export default function App() {
+  const [state, setState] = useState({ checked: {} });
+  //check if player has won
+  const hasWon = checked => {
+    const range = [0, 1, 2, 3 ,4];
+    return (
+      //check columns
+      undefined !==
+          range.find(row => range.every(column => checked[row * 5 + column])) ||
+      //check rows
+      undefined !==
+          range.find(column => range.every(row => checked[row * 5 + column])) ||
+      //check diagonal
+        range.every(index => checked[index * 5 + index]) || 
+        range.every(index => checked[index * 5 + 4 - index])
+    );
+  };
+  //win checking done via state
+  const winToggle = id => 
+    setState(state => {
+      const checked = { ...state.checked, [id]: !state.checked[id] };
+      const win = hasWon(checked);
+      return {
+        ...state,
+        checked,
+        win
+      };
+    });
+
+  return (
+    <div className="background">
+    <div className="App">
+      <h1 className="heading">Zoom Call Bingo</h1>
+
+      <div className="wrapper">
+        {console.log(shuffled)}
+      {
+        //map shuffled array to populate tiles
+        Object.keys(shuffled).map(id => (
+          <Tile key={id} id={id} isSet={!!state.checked[id]} onToggle={() => winToggle(id)} >
+            {shuffled[id]}
+          </Tile>
+        ))
+      }
+      </div>
+      
+      {//win triggers fireworks
+      state.win ? <Fireworks /> : null
+      }
+    </div>
+    </div>
+  );
+}
